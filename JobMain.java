f04d51ebbd9387f6c27f00b6457c20772446b448
cippo95 <cippo1995@gmail.com>
2021-11-23 11:01:46 +0100

    checkpoint, prova checkbox sul readme

--- JobMain.java
+++ JobMain.java
@@ -1,22 +1,44 @@
 import java.util.*;
-public class JobMain{
-	public static void main(String[] args){
-		int size = Integer.parseInt(args[0]);
+public class JobMain {
+	public static void main(String[] args) {	
+		int size = 0;
+		boolean debug = false;
+		//boolean random = false;
+		/*
+		 * Argomenti riga di comando:
+		 * 1) Creazione dei job: -j INT,
+		 * 2) Attivare più stampe per debug: -debug
+		 */
+		for (int i = 0; i < args.length; i++) {
+			String argument = args[i];
+			if (argument.equals("-j")) {
+				size = Integer.parseInt(args[i+1]);
+				i++;
+			} else if (argument.equals("-debug")) {
+				debug = true;
+			} else {
+				System.err.println("Illegal parameter usage");
+				return;
+			}
+		}
+		if (debug) {System.out.println("Debug is activated!");}
+		if (size == 0) { 
+			System.out.println("Numero dei job non specificato:"+
+			" usa -j INT come argomento.");
+			return;
+		}
 		JobIndexed[] job = new JobIndexed[size];
 		Random random = new Random();
 		int[] indexes = new int[size];
-		for(int i = 0; i < size; i++){
-			job[i] = new JobIndexed(random.nextInt(10),random.nextInt(10),i);
+		System.out.println("Lavori assegnati:");
+		for (int i = 0; i < size; i++) {
+			job[i] = new JobIndexed(random.nextInt(100),random.nextInt(100),i);
 			System.out.println(job[i]);
 		}
-		//controllare matrice e sommare "uni" per avere il rank
-		//ciclo di durata numero dei job, se job[i] rank 0 allora faccio add a una lista
-		//faccio sort per il nostro parametro, scelgo il primo, poi devo valutare di nuovo
-		//la matrice per andare a sottrare il rank a chi stava aspettando chi è uscito
-		//e poi aggiornata la lista rifaccio il sort e tolgo di nuovo il primo e così via. 
-		//fine.
 		RandomDagGenerator generator = new RandomDagGenerator(size);
 		int [][] array = generator.getRandomDag();
+		System.out.println();
+		System.out.println("Matrice triangolare delle precedenze");
 		System.out.println(Arrays.deepToString(array));
 		int [] priorities = new int[size];
 		int sum = 0;
@@ -27,6 +49,8 @@ public class JobMain{
 			priorities[i] = sum;
 			sum = 0;
 		}	
+		System.out.println();
+		System.out.println("Grado di priorità iniziale:");
 		System.out.println(Arrays.toString(priorities));
 		List<JobIndexed> ready = new ArrayList<JobIndexed>();
 		//creazione insieme iniziale
@@ -38,9 +62,11 @@ public class JobMain{
 		for(int i = 0; i < size ; i++){
 			//riordino insieme
 			Collections.sort(ready);
-			System.out.println(ready);
+			if( debug ) System.out.println(ready);
 			//copio indice primo lavoro
 			indexes[i] = ready.get(0).getIndex();
+			//controllo che abbia grado 0, se non è così priorità != dag
+			if(
 			//diminuire il rank a chi aspettava chi è uscito
 			for(int j = 0; j < size; j++){
 				if( array[j][indexes[i]]==1 ){
@@ -48,7 +74,7 @@ public class JobMain{
 				}
 			}
 			priorities[indexes[i]]--;
-			System.out.println(Arrays.toString(priorities));
+			if( debug ) System.out.println(Arrays.toString(priorities));
 			//aggiungere i nuovi job grado 0
 			for(int k = 0 ; k < size ; k++){
 				if( priorities[k]==0 && !ready.contains(job[k])){
@@ -57,8 +83,10 @@ public class JobMain{
 			}	
 			//elimino il lavoro dalla lista
 			ready.remove(0);
-			System.out.println(ready);
+			if( debug ) System.out.println(ready);
 		}
+		System.out.println();
+		System.out.println("Ordine di esecuzione:");
 		System.out.println(Arrays.toString(indexes));
 	}
 }
