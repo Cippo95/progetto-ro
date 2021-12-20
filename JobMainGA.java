9eda278825e9cfb1f0581f1b1cb32fd5b8ae1fa6
cippo95 <cippo1995@gmail.com>
2021-12-20 18:50:15 +0100

    checkpoint

--- JobMainGA.java
+++ JobMainGA.java
@@ -156,35 +156,38 @@ public class JobMainGA {
       			System.out.println("Errore di scrittura sul file.");
       			e.printStackTrace();
 		}
-		
 		//soluzione greedy
 		indexes = Greedy(jobs, matrix, debug);
+		System.out.println();
 		System.out.println("Soluzione Greedy");
-		System.out.println(Arrays.toString(indexes));
-		System.out.println(Fitness(jobs, indexes));
-		
+		if (fileLoad) {
+			Print(jobs, indexes, fileLoad, jobsNames);
+		} else {
+			Print(jobs, indexes);
+		}
 		//soluzione genetica
-		System.out.println("Vuoi provare algoritmo genetico?");
-		System.out.println("Può metterci molto tempo a seconda dell'istanza, in caso fare ctrl+c. (si/no)");
+		System.out.println();
+		System.out.println("Vuoi provare l'algoritmo genetico? Può metterci molto.");
+		System.out.println("In caso fare ctrl+c: rispondi 'si' per continuare.");
 		Scanner sc = new Scanner(System.in);
-		String a= sc.next();
+		String a = sc.next();
 		if (a.equals("si")) { 
-			indexes = GeneticAlg(jobs, matrix, debug);
+			indexes = GeneticAlg(jobs, matrix, false);
+			System.out.println();
 			System.out.println("Soluzione Genetica");
-			System.out.println(Arrays.toString(indexes));
-			System.out.println(Fitness(jobs, indexes));
-			if(Admissibility(indexes, matrix)) { 
-				System.out.println("controllo ok"); 
-			} else { 
-				System.out.println("non ok");
+			if (fileLoad) {
+				Print(jobs, indexes, fileLoad, jobsNames);
+			} else {
+				Print(jobs, indexes);
 			}
 		}
 	}
-	
+	/*
+	Algoritmo genetico: genera una popolazione di cui un elemento è la 
+	soluzione greedy, il resto sono soluzioni random ammissibili
+	*/
 	public static int[] GeneticAlg(List<Job> jobs, int[][] matrix, boolean debug) {
-		//popolazione
 		List<int[]> population = new ArrayList<int[]>();
-		//soluzione greedy
 		int[] indexes = Greedy(jobs, matrix, debug);
 		int[] best = indexes.clone();
 		population.add(indexes);
@@ -194,63 +197,28 @@ public class JobMainGA {
 		int bestFit = Fitness(jobs, indexes);
 		int[] priorities = Priorities(jobsNumber, matrix);
 		Random random = new Random();
-		//popolo di 20, 19 buone enumerazioni
-		for (int i = 0; i < 20-1; i++) {
+		//popolo di 200, 199 buone enumerazioni, come paper
+		for (int i = 0; i < 199-1; i++) {
 			indexes = GoodEnum(jobs, matrix, debug);
 			population.add(indexes);
 		}
-		/*
-		System.out.println(Arrays.toString(indexes));
-		int fitness = Fitness(jobs, indexes);
-		System.out.println(fitness);
-		if (Admissibility(indexes, matrix)) {
-			System.out.println("Soluzione ammissibile!");
-		} else {
-			System.out.println("Soluzione non ammissibile!");
-		}
-		
-		int[] indexesNew = localSearch(jobs, indexes, matrix);
-		fitness = Fitness(jobs, indexesNew);
-		System.out.println(fitness);
-		System.out.println(Arrays.toString(indexesNew));
-		if (Admissibility(indexes, matrix)) {
-			System.out.println("Soluzione ammissibile!");
-		} else {
-			System.out.println("Soluzione non ammissibile!");
-		}
-		*/
 		int[] child = new int[jobsNumber];
+		//100*n generazioni come paper
 		for(int gen = 0; gen < 100*jobsNumber; gen++) {
-			//System.out.println();
-			//System.out.println(gen + " best "+ bestFit);
-			//System.out.println("popolazione");
-			//for (int i = 0; i < population.size(); i++) {
-			//	System.out.println(Arrays.toString(population.get(i)));
-			//}
-			//primo e secondo genitore
-			
-
-			
 			int first = random.nextInt(population.size());
 			//System.out.println("primo genitore" + Arrays.toString(population.get(first)));
 			int second = random.nextInt(population.size());
 			//System.out.println("secondo genitore" + Arrays.toString(population.get(second)));
 			//punto di crossover
-			int point = random.nextInt(jobsNumber-1);
+			int point = random.nextInt(jobsNumber);
 			//System.out.println(point);
-			
-			//figlio
-
-			
-			//se già presenti nella prima parte non nella seconda
+			//job da evitare, serve per ordine relativo
 			List<Integer> avoidedJobs = new ArrayList<Integer>();
-			
 			for(int i = 0; i < point; i++) {
 				child[i] = population.get(first)[i];
 				avoidedJobs.add(child[i]);
 			}
 			//System.out.println("evita" + avoidedJobs);
-			int j = 0;
 			for(int i = 0; i < jobsNumber; i++) {
 				int candidate = population.get(second)[i];
 				//System.out.println("candidato" + candidate);
@@ -263,14 +231,13 @@ public class JobMainGA {
 			if(Admissibility(child, matrix)){
 				//calcolo fitness
 				int childFit = Fitness(jobs, child);
-				//System.out.println("fit figlio prima ls: "+ childFit);
+				//System.out.println("childFit pre ls: "+ childFit);
 				//ricerca locale sul figlio per miglioramento
 				for (int s = 0; s < jobsNumber; s++){
 					child = localSearch(jobs, child, matrix);
 				}
 				childFit = Fitness(jobs, child);
-				//System.out.println("fit figlio dopo ls: " + childFit);
-				//System.out.println("fit child " + childFit);
+				//System.out.println("childFit post ls: " + childFit);
 				//se migliore di tutti
 				if(childFit < bestFit) {
 					bestFit = childFit;
@@ -279,7 +246,7 @@ public class JobMainGA {
 				int worstFit = Fitness(jobs, population.get(0));
 				int worst = 0;
 				for (int i = 0; i < population.size(); i++) {
-					if( Fitness(jobs, population.get(i)) > worstFit ) {
+					if(Fitness(jobs, population.get(i)) > worstFit) {
 						worst = i;
 					}
 				}
@@ -291,31 +258,38 @@ public class JobMainGA {
 		}		
 		return best;	
 	}
-	
-	public static int[] localSearch(List<Job> jobs, int[] indexes, int[][] matrix ) {
-		//System.out.println("ricerca locale");
+	/*
+	* Algoritmo di ricerca locale: effettua swap in maniera esaustiva, se 
+	* trova miglioramento ammissibile sostituisce la soluzione.
+	*/
+	public static int[] localSearch(List<Job> jobs, int[] indexes, int[][] matrix) {
+		//indexesCopy mi serve per fare gli swap
 		int[] indexesCopy = indexes.clone();
+		//best inizialmente è l'ordine di partenza
 		int[] best = indexes.clone();
-		int item;
+		//fitness del ordine di partenza
 		int fitness = Fitness(jobs, indexes);
 		for (int i = 0; i < indexes.length; i++) {
 			for (int j = 0; j < indexes.length; j++) {
+				//reinizializzo indexesCopy uguale a start
 				indexesCopy = indexes.clone();
+				//swap tra due elementi
 				indexesCopy[i] = indexes[j];
 				indexesCopy[j] = indexes[i];
+				//se ammissibile, controllo fitness
 				if (Admissibility(indexesCopy, matrix)) { 
-					if ( Fitness(jobs, indexesCopy) < fitness ) {
+					if (Fitness(jobs, indexesCopy) < fitness) {
 						best = indexesCopy.clone();
 					}
 				}
-				//System.out.println(Arrays.toString(indexesCopy));
 			}
 		}
 		return best;
 	}
-	
+	/*
+	* Algoritmo di calcolo delle priorità
+	*/
 	public static int[] Priorities(int jobsNumber, int[][] matrix) {
-		//CALCOLO LE PRIORITÀ DEI JOB
 		int jobPrio = 0;
 		int[] priorities = new int[jobsNumber];
 		for (int i = 0; i < jobsNumber; i++) {
@@ -327,26 +301,32 @@ public class JobMainGA {
 		}
 		return priorities;
 	}
-	
+	/*
+	* Algoritmo di controllo ammissibilità, controlla la buona enum
+	*/
 	public static boolean Admissibility(int[] indexes, int[][] matrix) {
 		int jobsNumber = indexes.length;
 		int[] priorities = Priorities(jobsNumber, matrix);
 		for (int i = 0; i < jobsNumber; i++) {
-			//per ogni turno controllo che sia stato scelto un job grado 0
+			//per ogni turno controllo che sia job grado 0
 			if (priorities[indexes[i]] == 0) {
 				for (int j = 0; j < jobsNumber; j++) {
-					//diminuisco grado a chi aspettava il job
+					//diminuisco grado a chi aspettava
 					if(matrix[j][indexes[i]] == 1) {
 						priorities[j]--;
 					}
 				}
 			} else {
+				//non ho buona enum
 				return false;
 			}
 		}
+		//ho buona enum
 		return true;
 	}
-	
+	/*
+	* Algoritmo di calcolo fitness
+	*/
 	public static int Fitness(List<Job> jobs, int[] indexes) {
 		int jobsNumber = indexes.length;
 		int wait = 0;
@@ -357,21 +337,13 @@ public class JobMainGA {
 		}
 		return weightedCompletions;
 	}
-	
+	/*
+	* Algoritmo di stampa ordine, tempi di attesa etc.
+	*/
 	public static void Print(List<Job> jobs, int[] indexes) { 
-	int jobsNumber = indexes.length;
-	System.out.println();
+		int jobsNumber = indexes.length;
 		System.out.println("Ordine di esecuzione:");
-		//if (fileLoad) {
-		//	String[] indexesToNames = new String[jobsNumber];
-		//	for (int i = 0; i < jobsNumber; i++) {
-		//		indexesToNames[i] = jobsNames.get(indexes[i]);
-		//	}
-		//	System.out.println(Arrays.toString(indexesToNames));
-		//} else {
-			System.out.println(Arrays.toString(indexes));
-		//}
-		System.out.println();
+		System.out.println(Arrays.toString(indexes));
 		System.out.println("Tempi di attesa per job");
 		int wait = 0;
 		int tw = 0;
@@ -382,46 +354,41 @@ public class JobMainGA {
 			tw += wait * jobs.get(indexes[i]).getValue();
 		}
 		System.out.println(Arrays.toString(waitingTime));
-		System.out.println();
-		System.out.println("Somma pesata tempi completamento jobs: " + tw);
+		System.out.println("Funzione obiettivo: " + tw);
 	}
-	
-	
-	//Algoritmo greedy
+		/*
+	* Algoritmo di stampa ordine, tempi di attesa etc.
+	*/
+	public static void Print(List<Job> jobs, int[] indexes, boolean fileLoad, List<String> jobsNames) { 
+		int jobsNumber = indexes.length;
+		System.out.println("Ordine di esecuzione:");
+		String[] indexesToNames = new String[jobsNumber];
+		for (int i = 0; i < jobsNumber; i++) {
+		indexesToNames[i] = jobsNames.get(indexes[i]);
+		}
+		System.out.println(Arrays.toString(indexesToNames));
+		System.out.println("Tempi di attesa per job");
+		int wait = 0;
+		int tw = 0;
+		int[] waitingTime = new int[jobsNumber];
+		for (int i = 0; i < jobsNumber; i++) {
+			wait += jobs.get(indexes[i]).getSpan();
+			waitingTime[i] = wait;
+			tw += wait * jobs.get(indexes[i]).getValue();
+		}
+		System.out.println(Arrays.toString(waitingTime));
+		System.out.println("Somma pesata: " + tw);
+	}
+	/*
+	* Algoritmo greedy
+	*/
 	public static int[] Greedy(List<Job> jobs, int[][] matrix, boolean debug) {
 		//variabili
 		int jobsNumber = jobs.size();
 		List<Job> readyJobs = new ArrayList<Job>();
-		int[] priorities = new int[jobsNumber];
+		int[] priorities = Priorities(jobsNumber, matrix);
 		int jobPrio = 0;
 		int[] indexes = new int[jobsNumber];
-		/*
-		System.out.println("Lavori assegnati:");
-		for (int i = 0; i < jobsNumber; i++) {
-			System.out.println(jobs.get(i));
-		}
-		
-		System.out.println();
-		System.out.println("Matrice delle precedenze");
-		//più leggibile stampata per righe
-		for (int i = 0; i < jobsNumber; i++) {
-			System.out.println(Arrays.toString(matrix[i]));
-		}
-		*/
-		//CALCOLO LE PRIORITÀ DEI JOB
-
-		for (int i = 0; i < jobsNumber; i++) {
-			for (int j = 0; j < jobsNumber; j++) {
-				jobPrio += matrix[i][j];
-			}
-			priorities[i] = jobPrio;
-			jobPrio = 0;
-		}
-		/*
-		System.out.println();
-		System.out.println("Grado di priorità iniziale:");
-		System.out.println(Arrays.toString(priorities));
-		*/
 		//aggiungo nuovi job grado 0 e parte algoritmo principale
 		for (int k = 0; k < jobsNumber; k++) {
 			if (priorities[k] == 0
@@ -469,42 +436,17 @@ public class JobMainGA {
 		}
 		return indexes;
 	}
-	//Algoritmo buona enumerazione
+	/*
+	* Algoritmo che genera soluzioni ammissibili random
+	*/
 	public static int[] GoodEnum(List<Job> jobs, int[][] matrix, boolean debug) {
-		//variabili
 		Random random = new Random();
 		int jobsNumber = jobs.size();
 		List<Job> readyJobs = new ArrayList<Job>();
-		int[] priorities = new int[jobsNumber];
+		int[] priorities = Priorities(jobsNumber, matrix);
 		int jobPrio = 0;
 		int[] indexes = new int[jobsNumber];
 		Job jobItem;
-		/*
-		System.out.println("Lavori assegnati:");
-		for (int i = 0; i < jobsNumber; i++) {
-			System.out.println(jobs.get(i));
-		}
-		
-		System.out.println();
-		System.out.println("Matrice delle precedenze");
-		//più leggibile stampata per righe
-		for (int i = 0; i < jobsNumber; i++) {
-			System.out.println(Arrays.toString(matrix[i]));
-		}
-		*/
-		//CALCOLO LE PRIORITÀ DEI JOB
-
-		for (int i = 0; i < jobsNumber; i++) {
-			for (int j = 0; j < jobsNumber; j++) {
-				jobPrio += matrix[i][j];
-			}
-			priorities[i] = jobPrio;
-			jobPrio = 0;
-		}
-		//System.out.println();
-		//System.out.println("Grado di priorità iniziale:");
-		//System.out.println(Arrays.toString(priorities));
-		//aggiungo nuovi job grado 0 e parte algoritmo principale
 		for (int k = 0; k < jobsNumber; k++) {
 			if (priorities[k] == 0
 				&& !readyJobs.contains(jobs.get(k))) {
@@ -552,6 +494,5 @@ public class JobMainGA {
 		}
 		return indexes;
 	}
-	//Genetico con local search
 }
 
